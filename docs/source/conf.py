c58f58124af77b4e93432d451dcfb0ae5b1bbcdf
starslayerx <starslayerx@outlook.com>
2021-03-16 11:20:06 +0800

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,10 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.

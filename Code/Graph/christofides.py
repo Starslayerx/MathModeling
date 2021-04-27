fc8a5617cfef3b9b72d2d9094f009c4c5bf44d74
Starslayerx <53030987+Starslayerx@users.noreply.github.com>
2021-04-27 15:58:49 +0800

    Create christofides.py

---
+++ Code/Graph/christofides.py
@@ -0,0 +1,45 @@
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+def christofides(distance_materix, num_vehicles, depot):
+    def print_solution(manager, routing, solution):
+        print('Objective: {} miles'.format(solution.ObjectiveValue()))
+        index = routing.Start(0)
+        plan_output = 'Route for vehicle 0:\n'
+        route_distance = 0
+        while not routing.IsEnd(index):
+            plan_output += ' {} ->'.format(manager.IndexToNode(index))
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+        plan_output += ' {}\n'.format(manager.IndexToNode(index))
+        print(plan_output)
+        plan_output += 'Route distance: {}miles\n'.format(route_distance)
+    data = {}
+    data['distance_matrix'] = distance_materix
+    data['num_vehicles'] = num_vehicles
+    data['depot'] = depot
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                           data['num_vehicles'], data['depot'])
+    routing = pywrapcp.RoutingModel(manager)
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.CHRISTOFIDES)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(manager, routing, solution)
